e86e26e723df37c24de2ec30a4d57a834150e3c5
dsgnrr <dsgnrr@users.noreply.github.com>
2023-03-30 12:14:39 +0300

    Добавьте файлы проекта.

---
+++ ASP 201/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using ASP_201.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP_201.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Intro()
+        {
+            return View();
+        }
+
+        public IActionResult Scheme()
+        {
+            ViewBag.data = "Data in ViewBag";       // Спосіб передачі даних
+            ViewData["data"] = "Data in ViewData";  // до представлення
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
